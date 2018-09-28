a0c30615f5b1c44ac3c37c489e0de3e9a5f32247
Mishochik <mixailoparin@gmail.com>
2018-09-28 11:02:15 +0500

    Выполнил задание

--- interactive JavaScript course/Lesson-2/index.js
+++ interactive JavaScript course/Lesson-2/index.js
@@ -38,27 +38,43 @@ switch (tel[0]) {
 }
 
 
+let success = false;
+let error = 'Неизвестная ошибка';
+if (tel.length == 11) {
+	switch (tel[0]) {
+		case '8':
+			success = true;
+			break;
+		case '7':
+			error = 'Первая цифра семь';
+			break;
+	}
+}
+// console.log(success);
+// console.log(error);
+
+const s = success ? 'Тест пройден' : 'Тест не пройден' + error;
+// console.log(s);
+
+
 // ---------- ДЗ ----------
 const victoryPoints = 21;
 let fedorPoints = 22;
 let petrPoints = 19;
 
 if (fedorPoints > victoryPoints) {
-	fedorPoints = false
+	fedorPoints = false;
 }
 if (petrPoints > victoryPoints) {
-	petrPoints = false
+	petrPoints = false;
 }
-if (fedorPoints > petrPoints){
-	console.log(fedorPoints)
-	console.log('Fedor')
-} else if (petrPoints > fedorPoints) {
-	console.log(petrPoints)
-	console.log('Petr')
-}else if (fedorPoints == petrPoints){
-	console.log(fedorPoints)
-	console.log(petrPoints)
-	console.log('Ничья')
-}
+fedorPoints === false && petrPoints === false ? console.log('Ничья, оба перебор') :
+	fedorPoints > petrPoints ? console.log('Победил Fedor ' + fedorPoints) :
+		petrPoints > fedorPoints ? console.log('Победил Petr ' + petrPoints) :
+			console.log('Ничья, по ' + fedorPoints);
+
+
+
+
 
 
