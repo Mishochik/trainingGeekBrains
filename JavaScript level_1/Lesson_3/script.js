f09bc388d90f8397c30ed32f34c6724d17d71fce
Mishochik <mixailoparin@gmail.com>
2018-11-29 23:36:54 +0500

    Выполнил часть заданий третьего урока

---
+++ JavaScript level_1/Lesson_3/script.js
@@ -0,0 +1,63 @@
+function isPrime(n) {
+    if (n < 2) {
+        return false
+    };
+    const q = Math.floor(Math.sqrt(n));
+    let i = 2;
+    while (i <= q) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++
+    }
+    return true;
+}
+
+function getPrimeNumbers(maximumNumber) {
+    if (maximumNumber < 2) {
+        return;
+    }
+    const result = [];
+    let i = 2;
+    while (i <= maximumNumber) {
+        if (isPrime(i)) {
+            result.push(i);
+        }
+        i++;
+    }
+    return result;
+}
+
+console.log(getPrimeNumbers(100));
+
+
+for (let i = 0; i < 10; console.log(i++)) { }
+
+
+// function building() {
+//     for (let i = 0; i < 20; i++) {
+//         let brick = 'x';
+//         for (let n = 0; n < i; n++) {
+//             brick += 'x';
+//         }
+//         console.log(brick);
+//     }
+// }
+// building();
+
+function building() {
+    for (let i = 0; i < 20; i++) {
+        let brick = 'x';
+        let a = ' ';
+        for (let n = 0; n < i; n++) {
+            brick += 'xx';
+        }
+        for (let m = 20; m > i; m--) {
+            brick = a + brick;
+        }
+        console.log(brick);
+    }
+}
+building();
+
+
