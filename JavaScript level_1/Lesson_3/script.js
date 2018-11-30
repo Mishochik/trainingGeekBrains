58b7f3cf6d42a2a584b3294c0bae7b438dc57153
Mishochik <mixailoparin@gmail.com>
2018-11-30 10:35:51 +0500

    Выполнил все задания третьего урока

--- JavaScript level_1/Lesson_3/script.js
+++ JavaScript level_1/Lesson_3/script.js
@@ -31,6 +31,20 @@ function getPrimeNumbers(maximumNumber) {
 console.log(getPrimeNumbers(100));
 
 
+var i = 0;
+do {
+    if (i === 0) {
+        console.log(`${i} - это ноль`);
+    } else if (i % 2 === 0) {
+        console.log(`${i} - четное число`);
+    } else {
+        console.log(`${i} - нечетное число`);
+    }
+
+    i++;
+} while (i <= 10);
+
+
 for (let i = 0; i < 10; console.log(i++)) { }
 
 
